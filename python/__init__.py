4942df438a573ea5198067d3f7d81d8f644930f9
Nick Foster <nick@ettus.com>
2012-06-13 14:34:42 -0700

    try/except around modes_flightgear so users w/o numpy can still run it.

--- python/__init__.py
+++ python/__init__.py
@@ -56,8 +56,13 @@ from modes_sql import modes_output_sql
 from modes_sbs1 import modes_output_sbs1
 from modes_kml import modes_kml
 from modes_raw_server import modes_raw_server
-from modes_flightgear import modes_flightgear
-from Quaternion import *
+#this is try/excepted in case the user doesn't have numpy installed
+try:
+    from modes_flightgear import modes_flightgear
+    from Quaternion import *
+except ImportError:
+    print "gr-air-modes warning: numpy not installed, FlightGear interface not supported"
+    pass
 
 # ----------------------------------------------------------------
 # Tail of workaround
